c0d3362db189b7af60e0adb7367c2b76803b7c7e
JerryDanks <jerrydanks@outlook.com>
2021-07-02 16:48:36 +0800

    Add project files.

---
+++ KafkaCustomer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace KafkaCustomer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World kafka consumer !");
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "8.135.118.30:9092",
+                GroupId = "foo",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var cancel = false;
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                var topic = "test";
+                consumer.Subscribe(topic);
+
+                while (!cancel)
+                {
+                    var consumeResult = consumer.Consume(CancellationToken.None);
+
+                    Console.WriteLine($"Consumer message: { consumeResult.Message.Value} topic: {consumeResult.Topic} Partition: {consumeResult.Partition}");
+                }
+
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ KafkaStart/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaStart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World Producer!");
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "8.135.118.30:9092",
+                ClientId = Dns.GetHostName(),
+            };
+
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                string topic = "test";
+                for (int i = 0; i < 100; i++)
+                {
+                    var msg = "message " + i;
+                    Console.WriteLine($"Send message:   value {msg}");
+                    var result = await producer.ProduceAsync(topic, new Message<Null, string> { Value = msg });
+                    Console.WriteLine($"Result: key {result.Key} value {result.Value} partition:{result.TopicPartition}");
+                    Thread.Sleep(500);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
